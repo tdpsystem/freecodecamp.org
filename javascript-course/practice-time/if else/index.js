1d7c3282a1665aa1a0b01a3e758114fb54db26d9
Skye Dixon <cdixon316@gmail.com>
2022-07-27 06:14:15 +0100

    modified the if else conditionals doc

--- javascript-course/practice-time/if else/index.js
+++ javascript-course/practice-time/if else/index.js
@@ -2,11 +2,11 @@ let age = 21;
 
 if (age < 6) {
     console.log("Free")
-} else if (age > 5 && age < 18) {
+} else if (age < 18) {
     console.log("Child Discount")
-} else if (age > 17 && age < 27) {
+} else if (age < 27) {
     console.log("Student Discount")
-} else if (age > 26 && age <= 66) {
+} else if (age <= 66) {
     console.log("Full Price")
 } else (age > 66); {
     console.log("Senior Discount")
