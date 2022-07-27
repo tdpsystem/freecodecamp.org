fac6aeaf52d57a1ffe06ca7aacf4561168e2a41c
Skye Dixon <cdixon316@gmail.com>
2022-07-27 06:27:11 +0100

    Loops and Arrays

---
+++ javascript-course/practice-time/Loops and arrays/index.js
@@ -0,0 +1,6 @@
+let largeCountries = ["Russia", "Canada", "China", "USA", "Brazil"]
+
+console.log ("The 5 largest countries in the world as of this year")
+for (let i = 0; i < largeCountries.length; i++) {
+    console.log("- " + largeCountries[i])
+}
