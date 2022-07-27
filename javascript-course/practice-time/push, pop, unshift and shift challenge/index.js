04ff8dc5d5d266ae04cb82d99633693eab30be9b
Skye Dixon <cdixon316@gmail.com>
2022-07-27 10:19:24 +0100

    push,pop,shift,unshift

---
+++ javascript-course/practice-time/push, pop, unshift and shift challenge/index.js
@@ -0,0 +1,9 @@
+let largeCountries = ["Tuvalu", "Canada", "China", "USA", "Monaco"];
+
+largeCountries.pop();
+largeCountries.push("Brazil");
+
+largeCountries.shift();
+largeCountries.unshift("Russia");
+
+console.log(largeCountries);
