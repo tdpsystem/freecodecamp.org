e9ff0c3aae2abc230c5965206cf083b03c59e76d
Skye Dixon <cdixon316@gmail.com>
2022-07-27 05:45:32 +0100

    building blackjack

---
+++ javascript-course/building-blackjack/index.js
@@ -0,0 +1,62 @@
+let player = {
+    name: "Skye",
+    chips: 149
+};
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": £" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    if (sum <= 20) {
+        message = "Draw another card?"
+    } else if (sum === 21) {
+        message = "you won."
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let aNewCard = getRandomCard()
+        sum += aNewCard
+        cards.push(aNewCard)
+        renderGame();
+    }
+}
