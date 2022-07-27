f74b23522c4a4c503258187a5fbabfb33d04cfce
Skye Dixon <cdixon316@gmail.com>
2022-07-27 05:53:12 +0100

    objects and function

---
+++ javascript-course/practice-time/objects and functions/index.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Skye",
+    age: 21,
+    country: "England"
+};
+
+function logData() {
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+};
+
+logData();
